369525526f5baeac2ae6839b9cb7e01cd0b17d1b
ShohanShabbir <c171011@ugrad.iiuc.ac.bm>
2021-08-04 02:16:26 +0600

    first commit

---
+++ validation.js
@@ -0,0 +1,19 @@
+function validation(){  
+    var id=document.form.email.value;  
+    var ps=document.form.pass.value;  
+    if(id.length=="" && ps.length=="") {  
+        alert("User Name and Password fields are empty");  
+        return false;  
+    }  
+    else  
+    {  
+        if(id.length=="") {  
+            alert("User Name is empty");  
+            return false;  
+        }   
+        if (ps.length=="") {  
+        alert("Password field is empty");  
+        return false;  
+        }  
+    }                             
+}
